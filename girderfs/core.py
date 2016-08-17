620ad4d5cf952397f35fcee00b06d71c294237a4
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2016-08-17 10:29:19 -0500

    cosmetics

--- girderfs/core.py
+++ girderfs/core.py
@@ -1,20 +1,25 @@
 # -*- coding: utf-8 -*-
+"""
+Core classes for FUSE based filesystem handling Girder's resources
+"""
+
 import os
-import six
 import time
 import pathlib
-import encodings.idna  # NOQA: http://stackoverflow.com/questions/9144724/
-import requests
 from stat import S_IFDIR, S_IFREG
 from errno import ENOENT
+# http://stackoverflow.com/questions/9144724/
+import encodings.idna  # NOQA pylint: disable=unused-import
+import requests
+import six
 from dateutil.parser import parse as tparse
 from fuse import Operations, LoggingMixIn, FuseOSError
 import girder_client
 
 
 def _lstrip_path(path):
-    pathObj = pathlib.Path(path)
-    return pathlib.Path(*pathObj.parts[1:])
+    path_obj = pathlib.Path(path)
+    return pathlib.Path(*path_obj.parts[1:])
 
 
 def _convert_time(strtime):
@@ -22,15 +27,23 @@ def _convert_time(strtime):
 
 
 class GirderFS(LoggingMixIn, Operations):
+    """
+    Base class for handling Girder's folders
 
-    def __init__(self, folderId, gc):
+    :param folder_id: Folder id
+    :type folder_id: str
+    :param gc: Authenticated instance of GirderClient
+    :type gc: girder_client.GriderClient
+    """
+
+    def __init__(self, folder_id, girder_cli):
         super(GirderFS, self).__init__()
-        self.folderId = folderId
-        self.gc = gc
+        self.folder_id = folder_id
+        self.girder_cli = girder_cli
         self.cache = {}
 
-    def _get_object_by_path(self, objId, path):
-        raw_listing = self._get_listing(objId)
+    def _get_object_by_path(self, obj_id, path):
+        raw_listing = self._get_listing(obj_id)
         folder = next((item for item in raw_listing['folders']
                        if item["name"] == path.parts[0]), None)
 
@@ -48,50 +61,50 @@ class GirderFS(LoggingMixIn, Operations):
 
         return FuseOSError(ENOENT)
 
-    def _get_listing(self, objId):
+    def _get_listing(self, obj_id):
         try:
-            return self.cache[objId]
+            return self.cache[obj_id]
         except KeyError:
             try:
-                self.cache[objId] = self.gc.get('folder/%s/listing' % objId)
+                self.cache[obj_id] = self.girder_cli.get('folder/%s/listing' % obj_id)
             except girder_client.HttpError:
-                self.cache[objId] = self.gc.get('item/%s/listing' % objId)
+                self.cache[obj_id] = self.girder_cli.get('item/%s/listing' % obj_id)
         finally:
-            return self.cache[objId]
+            return self.cache[obj_id]
 
     def getattr(self, path, fh=None):
         if path == '/':
             return dict(st_mode=(S_IFDIR | 0o755), st_nlink=2)
-        obj, objType = self._get_object_by_path(
-            self.folderId, _lstrip_path(path))
+        obj, obj_type = self._get_object_by_path(
+            self.folder_id, _lstrip_path(path))
 
-        if objType == 'folder':
-            st = dict(st_mode=(S_IFDIR | 0o755), st_nlink=2)
+        if obj_type == 'folder':
+            stat = dict(st_mode=(S_IFDIR | 0o755), st_nlink=2)
         else:
-            st = dict(st_mode=(S_IFREG | 0o644), st_nlink=1)
+            stat = dict(st_mode=(S_IFREG | 0o644), st_nlink=1)
         ctime = _convert_time(obj["created"])
         try:
             mtime = _convert_time(obj["updated"])
         except KeyError:
             mtime = ctime
-        st.update(dict(st_ctime=ctime, st_mtime=mtime,
-                       st_size=obj["size"], st_atime=time.time()))
-        return st
+        stat.update(dict(st_ctime=ctime, st_mtime=mtime,
+                         st_size=obj["size"], st_atime=time.time()))
+        return stat
 
     def read(self, path, size, offset, fh):
-        raise NotImplemented
+        raise NotImplementedError
 
     def readdir(self, path, fh):
         dirents = ['.', '..']
         if path == '/':
-            raw_listing = self._get_listing(self.folderId)
+            raw_listing = self._get_listing(self.folder_id)
         else:
-            obj, objType = self._get_object_by_path(
-                self.folderId, _lstrip_path(path))
+            obj, obj_type = self._get_object_by_path(
+                self.folder_id, _lstrip_path(path))
             raw_listing = self._get_listing(obj["_id"])
 
-        for objType in raw_listing.keys():
-            dirents += [_["name"] for _ in raw_listing[objType]]
+        for obj_type in raw_listing.keys():
+            dirents += [_["name"] for _ in raw_listing[obj_type]]
         return dirents
 
     # Disable unused operations:
@@ -110,20 +123,20 @@ class RESTGirderFS(GirderFS):
     """
     Filesystem for locally mounting a remote Girder folder
 
-    :param folderId: Folder id
-    :type folderId: str
+    :param folder_id: Folder id
+    :type folder_id: str
     :param gc: Authenticated instance of GirderClient
     :type gc: girder_client.GriderClient
     """
 
     def read(self, path, size, offset, fh):
-        obj, objType = self._get_object_by_path(
-            self.folderId, _lstrip_path(path))
+        obj, _ = self._get_object_by_path(
+            self.folder_id, _lstrip_path(path))
 
         content = six.BytesIO()
         req = requests.get(
-            '%sfile/%s/download' % (self.gc.urlBase, obj["_id"]),
-            headers={'Girder-Token': self.gc.token},
+            '%sfile/%s/download' % (self.girder_cli.urlBase, obj["_id"]),
+            headers={'Girder-Token': self.girder_cli.token},
             params={'offset': offset, 'endByte': offset + size})
         for chunk in req.iter_content(chunk_size=65536):
             content.write(chunk)
@@ -134,18 +147,18 @@ class LocalGirderFS(GirderFS):
     """
     Filesystem for mounting local Girder's FilesystemAssetstore
 
-    :param folderId: Folder id
-    :type folderId: str
+    :param folder_id: Folder id
+    :type folder_id: str
     :param gc: Authenticated instance of GirderClient
     :type gc: girder_client.GriderClient
     """
 
     def read(self, path, size, offset, fh):
-        obj, objType = self._get_object_by_path(
-            self.folderId, _lstrip_path(path))
+        obj, _ = self._get_object_by_path(
+            self.folder_id, _lstrip_path(path))
         fh = os.open(obj['path'], os.O_RDONLY)
         os.lseek(fh, offset, 0)
         return os.read(fh, size)
 
-    def release(self, path, fh):
+    def release(self, path, fh):  # pylint: disable=unused-argument
         return os.close(fh)
